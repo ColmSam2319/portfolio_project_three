d419dec132c8350a80e77129333f0d86fee626d0
ColmSam2319 <colmsammon13@gmail.com>
2022-07-22 06:12:09 +0000

    code for board

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+user = []
+user_guesses = []
+comp = []
+
+def make_board(board):
+    """
+    Make an empty 5*5 board
+    """
+    for i in range(5):
+        board.append([" O "]*5)
+    return board
+
+print("board")
