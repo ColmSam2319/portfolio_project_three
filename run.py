8a869b7edfd86c47c7bf61b7e7662c0a50850df4
ColmSam2319 <colmsammon13@gmail.com>
2022-07-22 06:28:10 +0000

    code to generate ships

--- run.py
+++ run.py
@@ -27,3 +27,18 @@ def random_number(r):
     Generate a random number between 0 and the length of the board minus one.
     """
     return randint(0, len(r)-1)
+
+
+def generate_ship_loc(board):
+    """
+    4 ships populated onto the board via a while loop
+    """
+    ship_num = 0
+    while ship_num < 4:
+        ship_num = 0
+        ship_col = random_number(board)
+        ship_row = random_number(board)
+        board[ship_row][ship_col] = " o "
+        for row in board:
+            ship_num += row.count(" o ")
+
