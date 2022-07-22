1ea77b35bb482e55da33c187a55bd9ccb3ea896d
ColmSam2319 <colmsammon13@gmail.com>
2022-07-22 06:17:14 +0000

    defined print board and random int

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ user = []
 user_guesses = []
 comp = []
 
+
 def make_board(board):
     """
     Make an empty 5*5 board
@@ -12,4 +13,17 @@ def make_board(board):
         board.append([" O "]*5)
     return board
 
-print("board")
+
+def print_board(board):
+    """
+    Print the board
+    """
+    for ind in board:
+        print(" ".join(ind))
+
+
+def random_number(r):
+    """
+    Generate a random number between 0 and the length of the board minus one.
+    """
+    return randint(0, len(r)-1)
